a6350b9c0a45118f98e42859edcbaba31cd324ca
Jérémy Christillin <jeremy@neoservice.ai>
2026-07-31 14:32:38 +0200

    [skip-build] frontend artifacts for the content library

--- builder/public/frontend/assets/userFonts-GgoZK2g0.js
+++
@@ -1,2 +0,0 @@
-import{B as e}from"./index-Co7uy0YA.js";const o=e({doctype:"User Font",fields:["font_name","font_file"],cache:"userFonts",start:0,pageLength:50,auto:!0});export{o as default};
-//# sourceMappingURL=userFonts-GgoZK2g0.js.map
