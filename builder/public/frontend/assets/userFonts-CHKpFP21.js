27e680970fa01d6c6c40470b3aa66964abe7ab84
Jérémy Christillin <jeremy@neoservice.ai>
2026-07-31 11:33:11 +0200

    [skip-build] frontend artifacts for the Create with AI modal

--- builder/public/frontend/assets/userFonts-CHKpFP21.js
+++
@@ -1,2 +0,0 @@
-import{B as e}from"./index-zgOjTVpc.js";const o=e({doctype:"User Font",fields:["font_name","font_file"],cache:"userFonts",start:0,pageLength:50,auto:!0});export{o as default};
-//# sourceMappingURL=userFonts-CHKpFP21.js.map
